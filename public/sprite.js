1bafd06bc03d2cee412b2d4acbfd88bc7e1fbc65
Jesse Hattabaugh <arkanciscan@gmail.com>
2021-04-12 23:47:15 -0700

    fixing score misposition on iOS?

--- public/sprite.js
+++ public/sprite.js
@@ -7,6 +7,7 @@ class Sprite extends LitElement {
 				backface-visibility: hidden; /* prevents blurry emojis */
 				contain: layout;
 				justify-self: center;
+				position: relative;
 				text-shadow: 0 0.05em 0.08em rgba(0, 0, 0, 0.5);
 				user-select: none;
 			}
